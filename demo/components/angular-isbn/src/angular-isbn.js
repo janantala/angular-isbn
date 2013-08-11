167c654b87bf3de20c3a174637f203182b4899b1
Jan Antala <jan@antala.sk>
2013-08-11 19:56:10 +0200

    isbn-13

--- demo/components/angular-isbn/src/angular-isbn.js
+++ demo/components/angular-isbn/src/angular-isbn.js
@@ -9,12 +9,16 @@
 
   var isbn = angular.module('ja.isbn', []);
 
-
-  isbn.directive('isbn10', function ($parse) {
+  isbn.directive('isbn10', function () {
     return {
       require: 'ngModel',
       link: function(scope, elm, attrs, ctrl) {
         ctrl.$parsers.unshift(function(value) {
+
+          if (!value || value && value.length === 0) {
+            ctrl.$setValidity('isbn10', true);
+            return value;
+          }
 
           var str = value.replace(/\d/g, '');
           var allowedCharacters = /^[ -]*$/;
@@ -26,7 +30,7 @@
 
           var isbn = value.replace(/\D/g, '');
           console.log(isbn);
-          if (isbn && isbn.length == 10) {
+          if (isbn && isbn.length === 10) {
             // it is valid
             ctrl.$setValidity('isbn10', true);
             return value;
@@ -39,4 +43,41 @@
       }
     };
   });
+
+
+  isbn.directive('isbn13', function () {
+    return {
+      require: 'ngModel',
+      link: function(scope, elm, attrs, ctrl) {
+        ctrl.$parsers.unshift(function(value) {
+
+          if (!value || value && value.length === 0) {
+            ctrl.$setValidity('isbn13', true);
+            return value;
+          }
+
+          var str = value.replace(/\d/g, '');
+          var allowedCharacters = /^[ -]*$/;
+
+          if (! allowedCharacters.test(str)) {
+            ctrl.$setValidity('isbn13', false);
+            return undefined;
+          }
+
+          var isbn = value.replace(/\D/g, '');
+          console.log(isbn);
+          if (isbn && isbn.length === 13) {
+            // it is valid
+            ctrl.$setValidity('isbn13', true);
+            return value;
+          } else {
+            // it is invalid, return undefined (no model update)
+            ctrl.$setValidity('isbn13', false);
+            return undefined;
+          }
+        });
+      }
+    };
+  });
+
 })();
