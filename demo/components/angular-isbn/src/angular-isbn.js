b287c7e7ae14d8de8f768f77888afd5248654b6e
Jan Antala <jan@antala.sk>
2013-08-11 20:37:41 +0200

    checksum

--- demo/components/angular-isbn/src/angular-isbn.js
+++ demo/components/angular-isbn/src/angular-isbn.js
@@ -9,6 +9,23 @@
 
   var isbn = angular.module('ja.isbn', []);
 
+  var ISBN10WEIGHTS = [10,9,8,7,6,5,4,3,2,1];
+  var ISBN13WEIGHTS = [1,3,1,3,1,3,1,3,1,3,1,3,1];
+
+  var checksum = function(arr, weights){
+    return arr
+      .reduce(function(a, x, i){
+        a.push([Number(x), weights[i]]);
+        return a;
+      }, [])
+      .reduce(function(sum, a){
+        return sum + a[0] * a[1];
+      }, 0);
+  };
+
+  console.log(checksum('8020401059'.split(''), ISBN10WEIGHTS) % 11 === 0);
+  console.log(checksum('9788072038848'.split(''), ISBN13WEIGHTS) % 10 === 0);
+
   isbn.directive('isbn10', function () {
     return {
       require: 'ngModel',
