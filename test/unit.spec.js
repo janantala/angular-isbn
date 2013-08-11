c2dc140339f7d4d56f03a8f6e4079c7cc0bba2ef
Jan Antala <jan@antala.sk>
2013-08-12 01:26:47 +0200

    isbn10 and isbn13 tests

--- test/unit.spec.js
+++ test/unit.spec.js
@@ -36,19 +36,124 @@ describe('ja.isbn', function() {
     );
 
     scope = $rootScope;
+
     $compile(elm)(scope);
     scope.$digest();
   }));
 
-  it('should be valid', function(){
+  it('should be valid by default', function(){
+    expect(scope.ISBNForm.isbn10.$valid).toBe(true);
+    expect(scope.ISBNForm.isbn13.$valid).toBe(true);
     expect(scope.ISBNForm.$valid).toBe(true);
   });
 
   describe('isbn10 directive', function() {
 
+    beforeEach(inject(function($rootScope) {
+      scope = $rootScope.$new();
+    }));
+
+    it('should be invalid - text input', function(){
+      scope.ISBNForm.isbn10.$setViewValue('invalid isbn');
+      expect(scope.ISBNForm.isbn10.$valid).toBe(false);
+      expect(scope.ISBNForm.$valid).toBe(false);
+    });
+
+    it('should be invalid - invalid isbn number', function(){
+      scope.ISBNForm.isbn10.$setViewValue('1234567890');
+      expect(scope.ISBNForm.isbn10.$valid).toBe(false);
+      expect(scope.ISBNForm.$valid).toBe(false);
+    });
+
+    it('should be invalid - short input', function(){
+      scope.ISBNForm.isbn10.$setViewValue('123');
+      expect(scope.ISBNForm.isbn10.$valid).toBe(false);
+      expect(scope.ISBNForm.$valid).toBe(false);
+    });
+
+    it('should be invalid - long input', function(){
+      scope.ISBNForm.isbn10.$setViewValue('123456789012345');
+      expect(scope.ISBNForm.isbn10.$valid).toBe(false);
+      expect(scope.ISBNForm.$valid).toBe(false);
+    });
+
+    it('should be invalid - string with number input', function(){
+      scope.ISBNForm.isbn10.$setViewValue('isbn: 1449344852');
+      expect(scope.ISBNForm.isbn10.$valid).toBe(false);
+      expect(scope.ISBNForm.$valid).toBe(false);
+    });
+
+    it('should be valid - number input', function(){
+      scope.ISBNForm.isbn10.$setViewValue('1449344852');
+      expect(scope.ISBNForm.isbn10.$valid).toBe(true);
+      expect(scope.ISBNForm.$valid).toBe(true);
+    });
+
+    it('should be valid - dashed input', function(){
+      scope.ISBNForm.isbn10.$setViewValue('1-4493-4485-2');
+      expect(scope.ISBNForm.isbn10.$valid).toBe(true);
+      expect(scope.ISBNForm.$valid).toBe(true);
+    });
+
+    it('should be valid - spaced input', function(){
+      scope.ISBNForm.isbn10.$setViewValue('1 4493 4485 2');
+      expect(scope.ISBNForm.isbn10.$valid).toBe(true);
+      expect(scope.ISBNForm.$valid).toBe(true);
+    });
   });
 
   describe('isbn13 directive', function() {
 
+    beforeEach(inject(function($rootScope) {
+      scope = $rootScope.$new();
+    }));
+
+    it('should be invalid - text input', function(){
+      scope.ISBNForm.isbn13.$setViewValue('invalid isbn');
+      expect(scope.ISBNForm.isbn13.$valid).toBe(false);
+      expect(scope.ISBNForm.$valid).toBe(false);
+    });
+
+    it('should be invalid - invalid isbn number', function(){
+      scope.ISBNForm.isbn13.$setViewValue('1234567890123');
+      expect(scope.ISBNForm.isbn13.$valid).toBe(false);
+      expect(scope.ISBNForm.$valid).toBe(false);
+    });
+
+    it('should be invalid - short input', function(){
+      scope.ISBNForm.isbn13.$setViewValue('123');
+      expect(scope.ISBNForm.isbn13.$valid).toBe(false);
+      expect(scope.ISBNForm.$valid).toBe(false);
+    });
+
+    it('should be invalid - long input', function(){
+      scope.ISBNForm.isbn13.$setViewValue('123456789012345');
+      expect(scope.ISBNForm.isbn13.$valid).toBe(false);
+      expect(scope.ISBNForm.$valid).toBe(false);
+    });
+
+    it('should be invalid - string with number input', function(){
+      scope.ISBNForm.isbn13.$setViewValue('isbn: 9781449344856');
+      expect(scope.ISBNForm.isbn13.$valid).toBe(false);
+      expect(scope.ISBNForm.$valid).toBe(false);
+    });
+
+    it('should be valid - number input', function(){
+      scope.ISBNForm.isbn13.$setViewValue('9781449344856');
+      expect(scope.ISBNForm.isbn13.$valid).toBe(true);
+      expect(scope.ISBNForm.$valid).toBe(true);
+    });
+
+    it('should be valid - dashed input', function(){
+      scope.ISBNForm.isbn13.$setViewValue('978-1-4493-4485-6');
+      expect(scope.ISBNForm.isbn13.$valid).toBe(true);
+      expect(scope.ISBNForm.$valid).toBe(true);
+    });
+
+    it('should be valid - spaced input', function(){
+      scope.ISBNForm.isbn13.$setViewValue('978 1 4493 4485 6');
+      expect(scope.ISBNForm.isbn13.$valid).toBe(true);
+      expect(scope.ISBNForm.$valid).toBe(true);
+    });
   });
 });
