dad36955dba0ab6a4d243e207b148f76f80238e8
Jan Antala <jan@antala.sk>
2013-08-12 00:19:08 +0200

    project structure

---
+++ test/unit.spec.js
@@ -0,0 +1,16 @@
+describe('ja.isbn', function() {
+
+  beforeEach(module('ja.isbn'));
+
+  it('should be valid', function(){
+
+  });
+
+  describe('isbn10 directive', function() {
+
+  });
+
+  describe('isbn13 directive', function() {
+
+  });
+});
